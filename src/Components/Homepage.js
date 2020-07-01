a3971838c5871a59006749d7f59abf965ee203e8
Ahmad1Hmasho <a1994h.sy@gmail.com>
2020-07-01 19:54:59 +0200

    start

---
+++ src/Components/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PoseNet from "react-posenet";
+
+const Homepage = () => {
+  console.log(PoseNet);
+  return (
+    <>
+      <PoseNet />
+    </>
+  );
+};
+
+export default Homepage;
